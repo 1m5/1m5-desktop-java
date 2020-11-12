f858312d02f9bef479533f4a0d7ab529883fa0ce
objectorange <brian@resolvingarchitecture.io>
2020-11-11 22:41:22 -0500

    update to new code base changes

--- src/main/java/io/onemfive/desktop/views/ops/network/idn/IDNView.java
+++ src/main/java/io/onemfive/desktop/views/ops/network/idn/IDNView.java
@@ -1,35 +1,38 @@
-package io.onemfive.desktop.views.ops.network.ims;
+package io.onemfive.desktop.views.ops.network.idn;
 
-import io.onemfive.core.ServiceStatus;
 import io.onemfive.desktop.components.TitledGroupBg;
 import io.onemfive.desktop.util.Layout;
 import io.onemfive.desktop.views.ActivatableView;
 import io.onemfive.desktop.views.TopicListener;
-import io.onemfive.network.NetworkState;
-import io.onemfive.util.Res;
-import io.onemfive.util.StringUtil;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import ra.common.network.NetworkState;
+import ra.common.network.NetworkStatus;
+import ra.common.service.ServiceStatus;
+import ra.util.Resources;
+import ra.util.StringUtil;
 
 import static io.onemfive.desktop.util.FormBuilder.*;
 
-public class IMSOpsView extends ActivatableView implements TopicListener {
+public class IDNView extends ActivatableView implements TopicListener {
 
     private GridPane pane;
     private int gridRow = 0;
 
+    private NetworkStatus networkStatus = NetworkStatus.NOT_INSTALLED;
     private ServiceStatus serviceStatus = ServiceStatus.NOT_INITIALIZED;
-    private String serviceStatusField = StringUtil.capitalize(serviceStatus.name().toLowerCase().replace('_', ' '));
+
+    private String serviceStatusField = StringUtil.capitalize(networkStatus.name().toLowerCase().replace('_', ' '));
     private TextField serviceStatusTextField;
 
-    private String imsFingerprint = Res.get("ops.network.notKnownYet");
-    private String imsAddress = Res.get("ops.network.notKnownYet");
+    private String imsFingerprint = Resources.get("ops.network.notKnownYet");
+    private String imsAddress = Resources.get("ops.network.notKnownYet");
 
     private TextField imsFingerprintTextField;
     private TextArea imsAddressTextField;
 
-    public IMSOpsView() {
+    public IDNView() {
         super();
     }
 
@@ -37,14 +40,14 @@ public class IMSOpsView extends ActivatableView implements TopicListener {
         LOG.info("Initializing...");
         pane = (GridPane)root;
 
-        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Res.get("ops.network.status"));
+        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Resources.get("ops.network.status"));
         GridPane.setColumnSpan(statusGroup, 1);
-        serviceStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.status.network"), serviceStatusField, Layout.FIRST_ROW_DISTANCE).second;
+        serviceStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.status.network"), serviceStatusField, Layout.FIRST_ROW_DISTANCE).second;
 
-        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Res.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
+        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Resources.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
         GridPane.setColumnSpan(localNodeGroup, 1);
-        imsFingerprintTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.1m5.fingerprintLabel"), imsFingerprint, Layout.TWICE_FIRST_ROW_DISTANCE).second;
-        imsAddressTextField = addCompactTopLabelTextAreaWithText(pane, imsAddress, ++gridRow, Res.get("ops.network.1m5.addressLabel"), true).second;
+        imsFingerprintTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.1m5.fingerprintLabel"), imsFingerprint, Layout.TWICE_FIRST_ROW_DISTANCE).second;
+        imsAddressTextField = addCompactTopLabelTextAreaWithText(pane, imsAddress, ++gridRow, Resources.get("ops.network.1m5.addressLabel"), true).second;
 
         LOG.info("Initialized");
     }
@@ -64,10 +67,10 @@ public class IMSOpsView extends ActivatableView implements TopicListener {
         if(object instanceof NetworkState) {
             LOG.info("NetworkState received to update model.");
             NetworkState networkState = (NetworkState)object;
-            if(this.serviceStatus != networkState.serviceStatus) {
-                this.serviceStatus = networkState.serviceStatus;
+            if(this.networkStatus != networkState.networkStatus) {
+                this.networkStatus = networkState.networkStatus;
                 if(serviceStatusField != null) {
-                    serviceStatusTextField.setText(StringUtil.capitalize(serviceStatus.name().toLowerCase().replace('_', ' ')));
+                    serviceStatusTextField.setText(StringUtil.capitalize(networkStatus.name().toLowerCase().replace('_', ' ')));
                 }
             }
             if(networkState.localPeer!=null) {
