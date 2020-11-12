39404d0458d2904e8efa3c03bbc6a50885c3087b
objectorange <brian@resolvingarchitecture.io>
2020-11-11 22:42:52 -0500

    upgraded to code base wide changes

--- src/main/java/io/onemfive/desktop/views/ops/network/satellite/SatelliteOpsView.java
+++ src/main/java/io/onemfive/desktop/views/ops/network/satellite/SatelliteOpsView.java
@@ -4,32 +4,35 @@ import io.onemfive.desktop.components.TitledGroupBg;
 import io.onemfive.desktop.util.Layout;
 import io.onemfive.desktop.views.ActivatableView;
 import io.onemfive.desktop.views.TopicListener;
-import io.onemfive.network.NetworkState;
-import io.onemfive.network.sensors.SensorStatus;
-import io.onemfive.util.Res;
-import io.onemfive.util.StringUtil;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import ra.common.network.NetworkState;
+import ra.common.network.NetworkStatus;
+import ra.common.service.ServiceStatus;
+import ra.util.Resources;
+import ra.util.StringUtil;
 
 import static io.onemfive.desktop.util.FormBuilder.*;
 
-public class SatelliteSensorOpsView extends ActivatableView implements TopicListener {
+public class SatelliteOpsView extends ActivatableView implements TopicListener {
 
     private GridPane pane;
     private int gridRow = 0;
 
-    private SensorStatus sensorStatus = SensorStatus.NOT_INITIALIZED;
+    private NetworkStatus sensorStatus = NetworkStatus.NOT_INSTALLED;
+    private ServiceStatus serviceStatus = ServiceStatus.NOT_INITIALIZED;
+
     private String sensorStatusField = StringUtil.capitalize(sensorStatus.name().toLowerCase().replace('_', ' '));
     private TextField sensorStatusTextField;
 
-    private String satelliteFingerprint = Res.get("ops.network.notKnownYet");
+    private String satelliteFingerprint = Resources.get("ops.network.notKnownYet");
     private TextField satelliteFingerprintTextField;
 
-    private String satelliteAddress = Res.get("ops.network.notKnownYet");
+    private String satelliteAddress = Resources.get("ops.network.notKnownYet");
     private TextArea satelliteAddressTextArea;
 
-    public SatelliteSensorOpsView() {
+    public SatelliteOpsView() {
         super();
     }
 
@@ -38,14 +41,14 @@ public class SatelliteSensorOpsView extends ActivatableView implements TopicList
         LOG.info("Initializing...");
         pane = (GridPane)root;
 
-        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Res.get("ops.network.status"));
+        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Resources.get("ops.network.status"));
         GridPane.setColumnSpan(statusGroup, 1);
-        sensorStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.status.sensor"), sensorStatusField, Layout.FIRST_ROW_DISTANCE).second;
+        sensorStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.status.sensor"), sensorStatusField, Layout.FIRST_ROW_DISTANCE).second;
 
-        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Res.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
+        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Resources.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
         GridPane.setColumnSpan(localNodeGroup, 1);
-        satelliteFingerprintTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.satellite.fingerprintLabel"), satelliteFingerprint, Layout.TWICE_FIRST_ROW_DISTANCE).second;
-        satelliteAddressTextArea = addCompactTopLabelTextAreaWithText(pane, satelliteAddress, ++gridRow, Res.get("ops.network.satellite.addressLabel"), true).second;
+        satelliteFingerprintTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.satellite.fingerprintLabel"), satelliteFingerprint, Layout.TWICE_FIRST_ROW_DISTANCE).second;
+        satelliteAddressTextArea = addCompactTopLabelTextAreaWithText(pane, satelliteAddress, ++gridRow, Resources.get("ops.network.satellite.addressLabel"), true).second;
 
         LOG.info("Initialized");
     }
@@ -65,8 +68,8 @@ public class SatelliteSensorOpsView extends ActivatableView implements TopicList
         if(object instanceof NetworkState) {
             LOG.info("NetworkState received to update model.");
             NetworkState networkState = (NetworkState)object;
-            if(this.sensorStatus != networkState.sensorStatus) {
-                this.sensorStatus = networkState.sensorStatus;
+            if(this.sensorStatus != networkState.networkStatus) {
+                this.sensorStatus = networkState.networkStatus;
                 if(sensorStatusField != null) {
                     sensorStatusTextField.setText(StringUtil.capitalize(sensorStatus.name().toLowerCase().replace('_', ' ')));
                 }
