2d91306d59099f714d73940981e4daf9ae547a85
objectorange <brian@resolvingarchitecture.io>
2020-11-11 22:41:45 -0500

    updated to new code base changes

--- src/main/java/io/onemfive/desktop/views/ops/network/lifi/LiFiOpsView.java
+++ src/main/java/io/onemfive/desktop/views/ops/network/lifi/LiFiOpsView.java
@@ -4,32 +4,32 @@ import io.onemfive.desktop.components.TitledGroupBg;
 import io.onemfive.desktop.util.Layout;
 import io.onemfive.desktop.views.ActivatableView;
 import io.onemfive.desktop.views.TopicListener;
-import io.onemfive.network.NetworkState;
-import io.onemfive.network.sensors.SensorStatus;
-import io.onemfive.util.Res;
-import io.onemfive.util.StringUtil;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import ra.common.network.NetworkState;
+import ra.common.network.NetworkStatus;
+import ra.util.Resources;
+import ra.util.StringUtil;
 
 import static io.onemfive.desktop.util.FormBuilder.addCompactTopLabelTextField;
 import static io.onemfive.desktop.util.FormBuilder.addTitledGroupBg;
 
-public class LiFiSensorOpsView extends ActivatableView implements TopicListener {
+public class LiFiOpsView extends ActivatableView implements TopicListener {
 
     private GridPane pane;
     private int gridRow = 0;
 
-    private SensorStatus sensorStatus = SensorStatus.NOT_INITIALIZED;
-    private String sensorStatusField = StringUtil.capitalize(sensorStatus.name().toLowerCase().replace('_', ' '));
-    private TextField sensorStatusTextField;
+    private NetworkStatus networkStatus = NetworkStatus.NOT_INSTALLED;
+    private String networkStatusField = StringUtil.capitalize(networkStatus.name().toLowerCase().replace('_', ' '));
+    private TextField networkStatusTextField;
 
-    private String lifiFriendlyName = Res.get("ops.network.notKnownYet");
+    private String lifiFriendlyName = Resources.get("ops.network.notKnownYet");
     private TextField lifiFriendlyNameTextField;
 
-    private String lifiAddress = Res.get("ops.network.notKnownYet");
+    private String lifiAddress = Resources.get("ops.network.notKnownYet");
     private TextField lifiAddressTextField;
 
-    public LiFiSensorOpsView() {
+    public LiFiOpsView() {
         super();
     }
 
@@ -38,14 +38,14 @@ public class LiFiSensorOpsView extends ActivatableView implements TopicListener 
         LOG.info("Initializing...");
         pane = (GridPane)root;
 
-        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Res.get("ops.network.status"));
+        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Resources.get("ops.network.status"));
         GridPane.setColumnSpan(statusGroup, 1);
-        sensorStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.status.sensor"), sensorStatusField, Layout.FIRST_ROW_DISTANCE).second;
+        networkStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.status.sensor"), networkStatusField, Layout.FIRST_ROW_DISTANCE).second;
 
-        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Res.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
+        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Resources.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
         GridPane.setColumnSpan(localNodeGroup, 1);
-        lifiFriendlyNameTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.lifi.friendlyNameLabel"), lifiFriendlyName, Layout.TWICE_FIRST_ROW_DISTANCE).second;
-        lifiAddressTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.lifi.addressLabel"), lifiAddress).second;
+        lifiFriendlyNameTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.lifi.friendlyNameLabel"), lifiFriendlyName, Layout.TWICE_FIRST_ROW_DISTANCE).second;
+        lifiAddressTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.lifi.addressLabel"), lifiAddress).second;
 
         LOG.info("Initialized");
     }
@@ -65,10 +65,10 @@ public class LiFiSensorOpsView extends ActivatableView implements TopicListener 
         if(object instanceof NetworkState) {
             LOG.info("NetworkState received to update model.");
             NetworkState networkState = (NetworkState)object;
-            if(this.sensorStatus != networkState.sensorStatus) {
-                this.sensorStatus = networkState.sensorStatus;
-                if(sensorStatusField != null) {
-                    sensorStatusTextField.setText(StringUtil.capitalize(sensorStatus.name().toLowerCase().replace('_', ' ')));
+            if(this.networkStatus != networkState.networkStatus) {
+                this.networkStatus = networkState.networkStatus;
+                if(networkStatusField != null) {
+                    networkStatusTextField.setText(StringUtil.capitalize(networkStatus.name().toLowerCase().replace('_', ' ')));
                 }
             }
             if(networkState.localPeer!=null) {
