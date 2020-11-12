1a03ec3afb399fce746ed19e256900e233eac768
objectorange <brian@resolvingarchitecture.io>
2020-11-11 21:58:27 -0500

    move service registration and message sending to BusClient

--- src/main/java/io/onemfive/desktop/BusClient.java
+++ src/main/java/io/onemfive/desktop/BusClient.java
@@ -1,11 +1,38 @@
 package io.onemfive.desktop;
 
 import ra.common.Envelope;
+import ra.common.service.ServiceNotAccessibleException;
+import ra.common.service.ServiceNotSupportedException;
+import ra.common.service.ServiceStatusObserver;
+import ra.servicebus.ServiceBus;
+
+import java.util.List;
+import java.util.Properties;
 
 public class BusClient {
 
-    public static void sendRequest(Envelope envelope) {
+    private static ServiceBus bus;
 
+    public static boolean sendRequest(Envelope envelope) {
+        return bus.send(envelope);
+    }
+
+    public static void registerService(Class serviceClass, Properties p, List<ServiceStatusObserver> observers)
+            throws ServiceNotAccessibleException, ServiceNotSupportedException {
+        bus.registerService(serviceClass, p, observers);
+    }
+
+    public static boolean start(Properties properties) {
+        bus = new ServiceBus();
+        return bus.start(properties);
+    }
+
+    public static boolean shutdown(boolean force) {
+        if(force) {
+            return bus.shutdown();
+        } else {
+            return bus.gracefulShutdown();
+        }
     }
 
 }
