1d74f30d755b3aaad3630f21fc20cbbd5ab9563d
objectorange <brian@resolvingarchitecture.io>
2020-11-11 22:52:42 -0500

    upgrade to new codebase

--- src/main/java/io/onemfive/desktop/views/ops/network/wifidirect/WifiDirectOpsView.java
+++ src/main/java/io/onemfive/desktop/views/ops/network/wifidirect/WifiDirectOpsView.java
@@ -4,32 +4,35 @@ import io.onemfive.desktop.components.TitledGroupBg;
 import io.onemfive.desktop.util.Layout;
 import io.onemfive.desktop.views.ActivatableView;
 import io.onemfive.desktop.views.TopicListener;
-import io.onemfive.network.NetworkState;
-import io.onemfive.network.sensors.SensorStatus;
-import io.onemfive.util.Res;
-import io.onemfive.util.StringUtil;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import ra.common.network.NetworkState;
+import ra.common.network.NetworkStatus;
+import ra.common.service.ServiceStatus;
+import ra.util.Resources;
+import ra.util.StringUtil;
 
 import static io.onemfive.desktop.util.FormBuilder.addCompactTopLabelTextField;
 import static io.onemfive.desktop.util.FormBuilder.addTitledGroupBg;
 
-public class WifiDirectSensorOpsView extends ActivatableView implements TopicListener {
+public class WifiDirectOpsView extends ActivatableView implements TopicListener {
 
     private GridPane pane;
     private int gridRow = 0;
 
-    private SensorStatus sensorStatus = SensorStatus.NOT_INITIALIZED;
+    private NetworkStatus sensorStatus = NetworkStatus.NOT_INSTALLED;
+    private ServiceStatus serviceStatus = ServiceStatus.NOT_INITIALIZED;
+
     private String sensorStatusField = StringUtil.capitalize(sensorStatus.name().toLowerCase().replace('_', ' '));
     private TextField sensorStatusTextField;
 
-    private String wifidirectFriendlyName = Res.get("ops.network.notKnownYet");
+    private String wifidirectFriendlyName = Resources.get("ops.network.notKnownYet");
     private TextField wifidirectFriendlynameTextField;
 
-    private String wifidirectAddress = Res.get("ops.network.notKnownYet");
+    private String wifidirectAddress = Resources.get("ops.network.notKnownYet");
     private TextField wifidirectAddressTextField;
 
-    public WifiDirectSensorOpsView() {
+    public WifiDirectOpsView() {
         super();
     }
 
@@ -38,14 +41,14 @@ public class WifiDirectSensorOpsView extends ActivatableView implements TopicLis
         LOG.info("Initializing...");
         pane = (GridPane)root;
 
-        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Res.get("ops.network.status"));
+        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 2, Resources.get("ops.network.status"));
         GridPane.setColumnSpan(statusGroup, 1);
-        sensorStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.status.sensor"), sensorStatusField, Layout.FIRST_ROW_DISTANCE).second;
+        sensorStatusTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.status.sensor"), sensorStatusField, Layout.FIRST_ROW_DISTANCE).second;
 
-        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Res.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
+        TitledGroupBg localNodeGroup = addTitledGroupBg(pane, ++gridRow, 3, Resources.get("ops.network.localNode"),Layout.FIRST_ROW_DISTANCE);
         GridPane.setColumnSpan(localNodeGroup, 1);
-        wifidirectFriendlynameTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.wifidirect.friendlyNameLabel"), wifidirectFriendlyName, Layout.TWICE_FIRST_ROW_DISTANCE).second;
-        wifidirectAddressTextField = addCompactTopLabelTextField(pane, ++gridRow, Res.get("ops.network.wifidirect.addressLabel"), wifidirectAddress).second;
+        wifidirectFriendlynameTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.wifidirect.friendlyNameLabel"), wifidirectFriendlyName, Layout.TWICE_FIRST_ROW_DISTANCE).second;
+        wifidirectAddressTextField = addCompactTopLabelTextField(pane, ++gridRow, Resources.get("ops.network.wifidirect.addressLabel"), wifidirectAddress).second;
 
         LOG.info("Initialized");
     }
@@ -65,8 +68,8 @@ public class WifiDirectSensorOpsView extends ActivatableView implements TopicLis
         if(object instanceof NetworkState) {
             LOG.info("NetworkState received to update model.");
             NetworkState networkState = (NetworkState)object;
-            if(this.sensorStatus != networkState.sensorStatus) {
-                this.sensorStatus = networkState.sensorStatus;
+            if(this.sensorStatus != networkState.networkStatus) {
+                this.sensorStatus = networkState.networkStatus;
                 if(sensorStatusField != null) {
                     sensorStatusTextField.setText(StringUtil.capitalize(sensorStatus.name().toLowerCase().replace('_', ' ')));
                 }
