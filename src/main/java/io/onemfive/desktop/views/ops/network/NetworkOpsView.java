b1ddb8a1b95d4ca0f3460f72381153733bcc01e9
objectorange <brian@resolvingarchitecture.io>
2020-11-11 22:05:27 -0500

    moved to common Resources and removed unused imports

--- src/main/java/io/onemfive/desktop/views/ops/network/NetworkOpsView.java
+++ src/main/java/io/onemfive/desktop/views/ops/network/NetworkOpsView.java
@@ -6,14 +6,14 @@ import io.onemfive.desktop.views.ActivatableView;
 import io.onemfive.desktop.views.View;
 import io.onemfive.desktop.views.home.HomeView;
 import io.onemfive.desktop.views.ops.OpsView;
-import io.onemfive.desktop.views.ops.network.bluetooth.BluetoothSensorOpsView;
-import io.onemfive.desktop.views.ops.network.fullspectrum.FullSpectrumRadioSensorOpsView;
-import io.onemfive.desktop.views.ops.network.i2p.I2PSensorOpsView;
-import io.onemfive.desktop.views.ops.network.ims.IMSOpsView;
-import io.onemfive.desktop.views.ops.network.lifi.LiFiSensorOpsView;
-import io.onemfive.desktop.views.ops.network.satellite.SatelliteSensorOpsView;
-import io.onemfive.desktop.views.ops.network.tor.TORSensorOpsView;
-import io.onemfive.desktop.views.ops.network.wifidirect.WifiDirectSensorOpsView;
+import io.onemfive.desktop.views.ops.network.bluetooth.BluetoothOpsView;
+import io.onemfive.desktop.views.ops.network.fullspectrum.FullSpectrumRadioOpsView;
+import io.onemfive.desktop.views.ops.network.i2p.I2POpsView;
+import io.onemfive.desktop.views.ops.network.idn.IDNView;
+import io.onemfive.desktop.views.ops.network.lifi.LiFiOpsView;
+import io.onemfive.desktop.views.ops.network.satellite.SatelliteOpsView;
+import io.onemfive.desktop.views.ops.network.tor.TOROpsView;
+import io.onemfive.desktop.views.ops.network.wifidirect.WifiDirectOpsView;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
@@ -50,21 +50,21 @@ public class NetworkOpsView extends ActivatableView {
 
         tabChangeListener = (ov, oldValue, newValue) -> {
             if (newValue == imsTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, IMSOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, IDNView.class);
             else if (newValue == torTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, TORSensorOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, TOROpsView.class);
             else if (newValue == i2pTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, I2PSensorOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, I2POpsView.class);
             else if (newValue == wifiDirectTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, WifiDirectSensorOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, WifiDirectOpsView.class);
             else if (newValue == bluetoothTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, BluetoothSensorOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, BluetoothOpsView.class);
             else if (newValue == satelliteTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, SatelliteSensorOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, SatelliteOpsView.class);
             else if (newValue == fsRadioTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, FullSpectrumRadioSensorOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, FullSpectrumRadioOpsView.class);
             else if (newValue == lifiTab)
-                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, LiFiSensorOpsView.class);
+                MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, LiFiOpsView.class);
         };
 
         LOG.info("Initialized.");
@@ -77,21 +77,21 @@ public class NetworkOpsView extends ActivatableView {
 
         Tab selectedItem = pane.getSelectionModel().getSelectedItem();
         if (selectedItem == imsTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, IMSOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, IDNView.class);
         else if (selectedItem == torTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, TORSensorOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, TOROpsView.class);
         else if (selectedItem == i2pTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, I2PSensorOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, I2POpsView.class);
         else if (selectedItem == wifiDirectTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, WifiDirectSensorOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, WifiDirectOpsView.class);
         else if (selectedItem == bluetoothTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, BluetoothSensorOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, BluetoothOpsView.class);
         else if (selectedItem == satelliteTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, SatelliteSensorOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, SatelliteOpsView.class);
         else if (selectedItem == fsRadioTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, FullSpectrumRadioSensorOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, FullSpectrumRadioOpsView.class);
         else if (selectedItem == lifiTab)
-            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, LiFiSensorOpsView.class);
+            MVC.navigation.navigateTo(HomeView.class, OpsView.class, NetworkOpsView.class, LiFiOpsView.class);
     }
 
     @Override
@@ -104,14 +104,14 @@ public class NetworkOpsView extends ActivatableView {
         final Tab tab;
         View view = MVC.loadView(viewClass);
 
-        if (view instanceof IMSOpsView) tab = imsTab;
-        else if (view instanceof TORSensorOpsView) tab = torTab;
-        else if (view instanceof I2PSensorOpsView) tab = i2pTab;
-        else if (view instanceof WifiDirectSensorOpsView) tab = wifiDirectTab;
-        else if (view instanceof BluetoothSensorOpsView) tab = bluetoothTab;
-        else if (view instanceof SatelliteSensorOpsView) tab = satelliteTab;
-        else if (view instanceof FullSpectrumRadioSensorOpsView) tab = fsRadioTab;
-        else if (view instanceof LiFiSensorOpsView) tab = lifiTab;
+        if (view instanceof IDNView) tab = imsTab;
+        else if (view instanceof TOROpsView) tab = torTab;
+        else if (view instanceof I2POpsView) tab = i2pTab;
+        else if (view instanceof WifiDirectOpsView) tab = wifiDirectTab;
+        else if (view instanceof BluetoothOpsView) tab = bluetoothTab;
+        else if (view instanceof SatelliteOpsView) tab = satelliteTab;
+        else if (view instanceof FullSpectrumRadioOpsView) tab = fsRadioTab;
+        else if (view instanceof LiFiOpsView) tab = lifiTab;
         else throw new IllegalArgumentException("Navigation to " + viewClass + " is not supported");
 
         if (tab.getContent() != null && tab.getContent() instanceof ScrollPane) {
