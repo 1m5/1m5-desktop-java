123bf256fc6a9abf346bd69e585bab298482569e
objectorange <brian@resolvingarchitecture.io>
2020-12-31 15:43:32 -0500

    added messages to text area

--- src/main/java/io/onemfive/desktop/views/community/social/SocialView.java
+++ src/main/java/io/onemfive/desktop/views/community/social/SocialView.java
@@ -1,6 +1,5 @@
 package io.onemfive.desktop.views.community.social;
 
-import io.onemfive.desktop.DesktopBusClient;
 import io.onemfive.desktop.components.TitledGroupBg;
 import io.onemfive.desktop.util.Layout;
 import io.onemfive.desktop.views.ActivatableView;
@@ -9,12 +8,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import ra.common.Envelope;
 import ra.common.identity.DID;
 import ra.util.Resources;
 
@@ -24,13 +19,13 @@ import static io.onemfive.desktop.util.FormBuilder.*;
 
 public class SocialView extends ActivatableView implements TopicListener {
 
-    private ObservableList<String> contactAddresses = FXCollections.observableArrayList();
+    private final ObservableList<String> contactAddresses = FXCollections.observableArrayList();
 
     private GridPane pane;
     private int gridRow = 0;
 
     private String contactAddress = Resources.get("communityView.social.contactAddress");
-    private TextField addressTextField;
+    private ComboBox<String> contactAddressList;
 
     private String messages = Resources.get("communityView.social.messages");
     private TextArea messagesTextArea;
@@ -42,26 +37,22 @@ public class SocialView extends ActivatableView implements TopicListener {
     private Button sendTextButton = new Button(sendText);
 
     private Label notes;
-
-    public void updateContacts(List<DID> contacts) {
-        contactAddresses.clear();
-        for(DID c : contacts) {
-            contactAddresses.add(c.getUsername() + ": "+c.getPublicKey().getAddress());
-        }
-    }
+    private int msgCount = 0;
 
     @Override
     protected void initialize() {
         LOG.info("Initializing...");
         pane = (GridPane)root;
 
-        TitledGroupBg statusGroup = addTitledGroupBg(pane, gridRow, 1, Resources.get("communityView.social.messaging"));
-        GridPane.setColumnSpan(statusGroup, 1);
-
-        addressTextField = addInputTextField(pane, ++gridRow, Resources.get("communityView.social.contactAddress"), Layout.FIRST_ROW_DISTANCE);
+        contactAddressList = addComboBox(pane, ++gridRow, Resources.get("communityView.social.contactAddress"));
+        contactAddressList.maxWidth(250d);
         // TODO: Load past messages for last selected conversation
+        // TODO: Change from text area to three columns
         messagesTextArea = addCompactTopLabelTextAreaWithText(pane, "", ++gridRow, Resources.get("communityView.social.messages"), true).second;
+        messagesTextArea.maxWidth(250d);
+
         textTextField = addInputTextField(pane, ++gridRow, Resources.get("communityView.social.enterText"));
+        textTextField.maxWidth(250d);
         sendTextButton = addPrimaryActionButton(pane, ++gridRow, Resources.get("shared.send"), Layout.FIRST_ROW_DISTANCE);
 
         TitledGroupBg notesGroup = addTitledGroupBg(pane, ++gridRow, 1, Resources.get("shared.notes"), Layout.TWICE_FIRST_ROW_DISTANCE);
@@ -74,26 +65,58 @@ public class SocialView extends ActivatableView implements TopicListener {
 
     @Override
     protected void activate() {
+        contactAddressList.setItems(contactAddresses);
         sendTextButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 LOG.info("sendTextButton clicked...");
                 String txtToSend = textTextField.getText();
                 LOG.info("Sending text: "+txtToSend);
+                msgCount++;
+                if(msgCount%2>0)
+                    modelUpdated("newLocalMessage", txtToSend);
+                else
+                    modelUpdated("newRemoteMessage", txtToSend);
 //                Envelope e = Envelope.documentFactory();
 //
 //                DesktopBusClient.deliver(e);
+                textTextField.setText(null);
             };
         });
     }
 
     @Override
     protected void deactivate() {
+        contactAddresses.clear();
         sendTextButton.setOnAction(null);
     }
 
     @Override
     public void modelUpdated(String topic, Object object) {
-
+        switch (topic) {
+            case "contacts": {
+                contactAddresses.clear();
+                for(DID c : (List<DID>)object) {
+                    contactAddresses.add(c.getUsername() + ": "+c.getPublicKey().getAddress());
+                }
+                break;
+            }
+            case "newRemoteMessage": {
+                if(messagesTextArea.getText().isEmpty())
+                    messagesTextArea.appendText("\t"+object);
+                else {
+                    messagesTextArea.appendText("\n\n\t" + object);
+                }
+                break;
+            }
+            case "newLocalMessage": {
+                if(messagesTextArea.getText().isEmpty())
+                    messagesTextArea.appendText((String)object);
+                else {
+                    messagesTextArea.appendText("\n\n" + object);
+                }
+                break;
+            }
+        }
     }
 }
