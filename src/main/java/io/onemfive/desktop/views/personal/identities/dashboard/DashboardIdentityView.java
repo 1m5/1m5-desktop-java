852633638aaaf8bc5d8b8f53c656ff1688961f13
objectorange <1m5@protonmail.com>
2022-03-03 10:10:55 -0500

    added DashboardIdentityView

---
+++ src/main/java/io/onemfive/desktop/views/personal/identities/dashboard/DashboardIdentityView.java
@@ -0,0 +1,12 @@
+package io.onemfive.desktop.views.personal.identities.dashboard;
+
+import io.onemfive.desktop.views.TopicListener;
+import io.onemfive.desktop.views.personal.identities.BaseIdentityView;
+
+public class DashboardIdentityView extends BaseIdentityView implements TopicListener {
+
+    @Override
+    public void modelUpdated(String topic, Object object) {
+
+    }
+}
