9327ecf8d435fe98777d35f8dbaf25f0dbe1403d
objectorange <1m5@protonmail.com>
2022-03-03 10:10:42 -0500

    added AddIdentityView

---
+++ src/main/java/io/onemfive/desktop/views/personal/identities/add/AddIdentityView.java
@@ -0,0 +1,12 @@
+package io.onemfive.desktop.views.personal.identities.add;
+
+import io.onemfive.desktop.views.TopicListener;
+import io.onemfive.desktop.views.personal.identities.BaseIdentityView;
+
+public class AddIdentityView  extends BaseIdentityView implements TopicListener {
+
+    @Override
+    public void modelUpdated(String topic, Object object) {
+
+    }
+}
