917824b241f1a5bdcf58c6ec2fb0fd6677fa2fe5
objectorange <1m5@protonmail.com>
2022-03-03 10:24:31 -0500

    added DetailsContactView

---
+++ src/main/java/io/onemfive/desktop/views/personal/contacts/details/DetailsContactView.java
@@ -0,0 +1,13 @@
+package io.onemfive.desktop.views.personal.contacts.details;
+
+import io.onemfive.desktop.views.ActivatableView;
+import io.onemfive.desktop.views.TopicListener;
+import io.onemfive.desktop.views.personal.contacts.BaseContactView;
+
+public class DetailsContactView extends BaseContactView implements TopicListener {
+
+    @Override
+    public void modelUpdated(String topic, Object object) {
+
+    }
+}
