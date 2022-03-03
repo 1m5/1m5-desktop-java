d04827778816c2e0592d6f9696833085ac4580e8
objectorange <1m5@protonmail.com>
2022-03-03 10:11:25 -0500

    added ListIdentityView

---
+++ src/main/java/io/onemfive/desktop/views/personal/identities/list/ListIdentityView.java
@@ -0,0 +1,12 @@
+package io.onemfive.desktop.views.personal.identities.list;
+
+import io.onemfive.desktop.views.TopicListener;
+import io.onemfive.desktop.views.personal.identities.BaseIdentityView;
+
+public class ListIdentityView extends BaseIdentityView implements TopicListener {
+
+    @Override
+    public void modelUpdated(String topic, Object object) {
+
+    }
+}
