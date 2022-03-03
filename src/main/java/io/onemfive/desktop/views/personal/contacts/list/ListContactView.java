f358e241a0984b443cfbb03b4a98131ec190f3b2
objectorange <1m5@protonmail.com>
2022-03-03 10:24:42 -0500

    added ListContactView

---
+++ src/main/java/io/onemfive/desktop/views/personal/contacts/list/ListContactView.java
@@ -0,0 +1,13 @@
+package io.onemfive.desktop.views.personal.contacts.list;
+
+import io.onemfive.desktop.views.ActivatableView;
+import io.onemfive.desktop.views.TopicListener;
+import io.onemfive.desktop.views.personal.contacts.BaseContactView;
+
+public class ListContactView extends BaseContactView implements TopicListener {
+
+    @Override
+    public void modelUpdated(String topic, Object object) {
+
+    }
+}
