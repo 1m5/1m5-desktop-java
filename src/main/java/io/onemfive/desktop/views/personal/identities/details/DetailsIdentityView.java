813ade1e1476cb73316a65d0431200436eb1ad04
objectorange <1m5@protonmail.com>
2022-03-03 10:11:07 -0500

    added DetailsIdentityView

---
+++ src/main/java/io/onemfive/desktop/views/personal/identities/details/DetailsIdentityView.java
@@ -0,0 +1,12 @@
+package io.onemfive.desktop.views.personal.identities.details;
+
+import io.onemfive.desktop.views.TopicListener;
+import io.onemfive.desktop.views.personal.identities.BaseIdentityView;
+
+public class DetailsIdentityView extends BaseIdentityView implements TopicListener {
+
+    @Override
+    public void modelUpdated(String topic, Object object) {
+
+    }
+}
