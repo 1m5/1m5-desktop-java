312740243c75a0b03b80f4a17013d185b833f163
objectorange <1m5@protonmail.com>
2022-03-17 10:33:10 -0400

    init impl identity list and dashboard

--- src/main/java/io/onemfive/desktop/views/personal/identities/dashboard/DashboardIdentityView.java
+++ src/main/java/io/onemfive/desktop/views/personal/identities/dashboard/DashboardIdentityView.java
@@ -3,6 +3,7 @@ package io.onemfive.desktop.views.personal.identities.dashboard;
 import io.onemfive.desktop.DesktopClient;
 import io.onemfive.desktop.components.AutoTooltipButton;
 import io.onemfive.desktop.components.PasswordTextField;
+import io.onemfive.desktop.components.TitledGroupBg;
 import io.onemfive.desktop.util.Layout;
 import io.onemfive.desktop.views.ActivatableView;
 import io.onemfive.desktop.views.TopicListener;
@@ -12,16 +13,22 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import ra.common.Envelope;
 import ra.common.Resources;
 import ra.common.identity.DID;
+import ra.common.network.ControlCommand;
+import ra.did.AuthenticateDIDRequest;
+import ra.did.DIDService;
 
-import static io.onemfive.desktop.views.personal.identities.details.DetailsIdentityView.ACTIVE_IDENTITY;
+import static io.onemfive.desktop.util.FormBuilder.*;
 import static java.util.Objects.nonNull;
 
 public class DashboardIdentityView extends ActivatableView implements TopicListener {
+
+    private static final String AUTHN_RESULT = "AUTHN_RESULT";
 
     private final String authNText = Resources.get("personalIdentitiesView.authn");
     private final String aliasPrompt = Resources.get("shared.alias");
@@ -32,67 +39,57 @@ public class DashboardIdentityView extends ActivatableView implements TopicListe
 
     private ComboBox<String> authNAliasComboBox;
     private PasswordTextField authNPwdText;
-    private Button authN;
+    private Button authNButton;
+    private Label errorLabel;
+    private String currentUsername = null;
 
     private ObservableList<String> identityAddresses = FXCollections.observableArrayList();
 
     @Override
     protected void initialize() {
-        super.initialize();
+        LOG.info("Initializing...");
+        pane = (GridPane)root;
+        TitledGroupBg createWalletGroup = addTitledGroupBg(pane, ++gridRow, 4, Resources.get("personalIdentitiesView.authn"), Layout.FIRST_ROW_DISTANCE);
+        GridPane.setColumnSpan(createWalletGroup, 1);
         // Authenticate Identity
-        authNAliasComboBox = new ComboBox<>();
+        authNAliasComboBox = addComboBox(pane, gridRow, Resources.get("personalIdentitiesView.username"));
         authNAliasComboBox.setItems(identityAddresses);
         authNAliasComboBox.getStyleClass().add("jfx-combo-box");
-        authNAliasComboBox.setValue("Anonymous");
+        authNAliasComboBox.setPromptText("Select username");
         authNPwdText = new PasswordTextField();
         authNPwdText.setVisible(false);
-        authN = new AutoTooltipButton(authNText);
-        authN.setDefaultButton(true);
-        authN.getStyleClass().add("action-button");
-        authN.setVisible(false);
-        pane.add(new VBox(Layout.GRID_GAP, authNAliasComboBox, authNPwdText, authN), 0, gridRow);
+        authNButton = new AutoTooltipButton(authNText);
+        authNButton.setDefaultButton(true);
+        authNButton.getStyleClass().add("action-button");
+        authNButton.setVisible(false);
+        errorLabel = addLabel(pane, ++gridRow, "");
+        errorLabel.setVisible(false);
+        pane.add(new VBox(Layout.GRID_GAP, authNAliasComboBox, authNPwdText, authNButton), 0, gridRow);
         LOG.info("Initialized.");
     }
 
     @Override
     protected void activate() {
         if(nonNull(DesktopClient.getCache().getActivePersonalDID())) {
-            authNAliasComboBox.setValue(DesktopClient.getCache().getActivePersonalDID().getUsername());
+            currentUsername = DesktopClient.getCache().getActivePersonalDID().getUsername();
+            authNAliasComboBox.setValue(currentUsername);
         }
         authNAliasComboBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                boolean isAnonymous = "Anonymous".equals(authNAliasComboBox.getValue());
-                authNPwdText.setVisible(!isAnonymous);
-                authN.setVisible(!isAnonymous);
+                if(authNAliasComboBox.getValue()==null) {
+
+                }
+
             }
         });
-        authN.setOnAction(new EventHandler<ActionEvent>() {
+        authNButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-//                if(!authNAliasTxt.getText().isEmpty() && !authNPwdText.getText().isEmpty()) {
-//                    Envelope e = Envelope.documentFactory();
-//                    // 3. Update UI
-//                    e.addRoute(DesktopBusClient.class, DesktopBusClient.OPERATION_NOTIFY_UI);
-//                    // 2. Load ordered Contacts
-//                    e.addRoute(DIDService.class, DIDService.OPERATION_GET_CONTACTS);
-//                    // 1. Authenticate
-//                    DID did = new DID();
-//                    did.setUsername(authNAliasTxt.getText());
-//                    did.setPassphrase(authNPwdText.getText());
-//                    did.setPassphrase2(authNPwdText.getText());
-//                    AuthNRequest ar = new AuthNRequest();
-//                    ar.keyRingUsername = did.getUsername();
-//                    ar.keyRingPassphrase = did.getPassphrase();
-//                    ar.alias = did.getUsername(); // use username as default alias
-//                    ar.aliasPassphrase = did.getPassphrase(); // just use same passphrase
-//                    ar.autoGenerate = true;
-//                    e.setDID(did);
-//                    e.addData(AuthNRequest.class, ar);
-//                    e.addRoute(KeyRingService.class, KeyRingService.OPERATION_AUTHN);
-//                    // Send
-//                    DesktopBusClient.deliver(e);
-//                }
+                errorLabel.setText(null);
+                errorLabel.setVisible(false);
+
+//                authenticate(username, passphrase);
             }
         });
     }
@@ -100,7 +97,7 @@ public class DashboardIdentityView extends ActivatableView implements TopicListe
     @Override
     protected void deactivate() {
         authNAliasComboBox.setOnAction(null);
-        authN.setOnAction(null);
+        authNButton.setOnAction(null);
     }
 
     @Override
@@ -108,13 +105,36 @@ public class DashboardIdentityView extends ActivatableView implements TopicListe
         LOG.info("Updating model with topic: "+topic);
         Envelope e = (Envelope)object;
         switch (topic) {
-            case ACTIVE_IDENTITY: {
-                DID did = (DID)e.getValue("activeIdentity");
-                if(nonNull(did)) {
+            case AUTHN_RESULT: {
+                if(e.getValue("authN")!=null) {
+                    // AuthN failed
+                    errorLabel.setText("Authentication failed.");
+                    errorLabel.setVisible(true);
+                } else {
+                    DID did = (DID)e.getData(DID.class);
                     DesktopClient.getCache().setActivePersonalDID(did);
+                    currentUsername = did.getUsername();
+                    authNPwdText.setText(null);
+                    authNPwdText.setVisible(false);
+                    authNButton.setVisible(false);
+                    errorLabel.setText(null);
+                    errorLabel.setVisible(false);
                 }
                 break;
-            }        }
+            }
+        }
         LOG.info("Model updated.");
+    }
+
+    private void authenticate(String username, String passphrase) {
+        AuthenticateDIDRequest request = new AuthenticateDIDRequest();
+        request.username = username;
+        request.passphrase = passphrase;
+        request.didType = DID.DIDType.IDENTITY;
+        request.external = false;
+        Envelope e = Envelope.documentFactory();
+        e.setCommandPath(ControlCommand.Send.name());
+        e.addData(AuthenticateDIDRequest.class, request);
+        e.addRoute(DIDService.class, DIDService.OPERATION_AUTHENTICATE);
     }
 }
--- src/main/java/io/onemfive/desktop/views/personal/identities/list/ListIdentityView.java
+++ src/main/java/io/onemfive/desktop/views/personal/identities/list/ListIdentityView.java
@@ -7,52 +7,74 @@ import io.onemfive.desktop.views.TopicListener;
 import io.onemfive.desktop.views.personal.identities.IdentitiesView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import ra.common.Envelope;
+import ra.common.Resources;
+import ra.common.identity.DID;
 import ra.common.network.ControlCommand;
 import ra.did.DIDService;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import static io.onemfive.desktop.DesktopClient.VIEW_NAME;
 import static io.onemfive.desktop.DesktopClient.VIEW_OP;
+import static io.onemfive.desktop.util.FormBuilder.addPrimaryActionButton;
+import static io.onemfive.desktop.util.FormBuilder.addTopLabelListView;
 
 public class ListIdentityView extends ActivatableView implements TopicListener {
 
     public static final String IDENTITIES_LIST = "IDENTITIES_LIST";
+    public static final String IDENTITY_DELETED = "IDENTITY_DELETED";
 
     private GridPane pane;
     private int gridRow = 0;
 
-    protected static ObservableList<Object> identityDIDs = FXCollections.observableArrayList();
-    protected static ListView<Object> identitiesList;
+    protected static ObservableList<Object> usernames = FXCollections.observableArrayList();
+    protected static ListView<Object> usernameListView;
 
-    private Button deleteIdentity;
+    private Button deleteButton;
 
     @Override
     protected void initialize() {
-        super.initialize();
-        // List Identities
-        identitiesList = new ListView<>();
-        identitiesList.setPrefSize(800, 250);
-        identitiesList.setItems(identityDIDs);
-        identitiesList.setEditable(false);
-        identitiesList.getStyleClass().add("listView");
-        VBox mainBox = new VBox(Layout.GRID_GAP, identitiesList);
-        pane.add(mainBox, 0, gridRow);
+        LOG.info("Initializing...");
+        pane = (GridPane)root;
 
+        // List Identities
+        usernameListView = addTopLabelListView(pane, gridRow, Resources.get("personalIdentitiesView.identities"), Layout.FIRST_ROW_DISTANCE).second;
+        usernameListView.setItems(usernames);
+        usernameListView.setEditable(false);
+        usernameListView.getStyleClass().add("listView");
+        deleteButton = addPrimaryActionButton(pane, ++gridRow, Resources.get("shared.create"), Layout.TWICE_FIRST_ROW_DISTANCE);
+        deleteButton.getStyleClass().add("action-button");
         LOG.info("Initialized.");
     }
 
     @Override
     protected void activate() {
-        super.activate();
+        deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Object username = usernameListView.getSelectionModel().getSelectedItem();
+                if(username!=null)
+                    deleteIdentity((String)username);
+            }
+        });
+        if(DesktopClient.getCache().getLocalIdentities()==null
+                || DesktopClient.getCache().getLocalIdentities().isEmpty()) {
+            updateIdentitiesList();
+        }
     }
 
     @Override
     protected void deactivate() {
-        super.deactivate();
+        deleteButton.setOnAction(null);
     }
 
     @Override
@@ -60,9 +82,37 @@ public class ListIdentityView extends ActivatableView implements TopicListener {
         LOG.info("Updating model with topic: "+topic);
         Envelope e = (Envelope)object;
         switch (topic) {
+            case IDENTITIES_LIST: {
+                List<DID> dids = (List<DID>)e.getValue("identities");
+                Collections.sort(dids, new Comparator<DID>() {
+                    @Override
+                    public int compare(DID o1, DID o2) {
+                        if(o1==null || o2==null)
+                            return 0;
+                        return o1.getUsername().compareTo(o2.getUsername());
+                    }
+                });
+                usernames.clear();
+                for(DID did : dids) {
+                    usernames.add(did.getUsername());
+                }
+                break;
+            }
+            case IDENTITY_DELETED: {
 
+                break;
+            }
         }
         LOG.info("Model updated.");
+    }
+
+    private void deleteIdentity(String username) {
+        Envelope e = Envelope.documentFactory();
+        e.setCommandPath(ControlCommand.Send.name());
+        e.addNVP(VIEW_NAME, IdentitiesView.class.getName());
+        e.addNVP(VIEW_OP, IDENTITY_DELETED);
+        e.addRoute(DIDService.class, DIDService.OPERATION_DELETE_IDENTITY);
+        DesktopClient.deliver(e);
     }
 
     private void updateIdentitiesList() {
